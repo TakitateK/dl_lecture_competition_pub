ea0206dfff1ef81c30eba0a047b48641016537c0
TakitateK <hongama77@g.ecc.u-tokyo.ac.jp>
2024-07-17 06:24:49 +0000

    preprocessed_datasets

--- src/preprocess_datasets.py
+++ src/preprocess_datasets.py
@@ -7,10 +7,11 @@ from mne.filter import filter_data
 from glob import glob
 
 class PreprocessedThingsMEGDataset(Dataset):
-    def __init__(self, split: str, data_dir: str = "preprocessed_data"):
+    def __init__(self, split: str, data_dir: str = "preprocessed_data", original_data_dir: str = "data"):
         assert split in ["train", "val", "test"], f"Invalid split: {split}"
         self.split = split
         self.data_dir = data_dir
+        self.original_data_dir = original_data_dir
         self.file_paths = glob(os.path.join(data_dir, f"{split}_X", "*.npy"))
         
     def __len__(self) -> int:
@@ -21,11 +22,11 @@ class PreprocessedThingsMEGDataset(Dataset):
         X = np.load(X_path).astype(np.float32)
         X = torch.from_numpy(X).float()
 
-        subject_idx_path = os.path.join(self.data_dir, f"{self.split}_subject_idxs", os.path.basename(X_path))
+        subject_idx_path = os.path.join(self.original_data_dir, f"{self.split}_subject_idxs", os.path.basename(X_path))
         subject_idx = torch.from_numpy(np.load(subject_idx_path))
 
         if self.split in ["train", "val"]:
-            y_path = os.path.join(self.data_dir, f"{self.split}_y", os.path.basename(X_path))
+            y_path = os.path.join(self.original_data_dir, f"{self.split}_y", os.path.basename(X_path))
             y = torch.from_numpy(np.load(y_path))
             return X, y, subject_idx
         else:
